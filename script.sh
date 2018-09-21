8db5943f31a03297e0dfdc7a49823280c875da32
Joel Fischer <jfjoel.fischer@gmail.com>
2018-09-21 19:45:39 +0200

    Updated script

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ printf "Update blocklist.txt\n"
 
 printf "Download AdBlock Lists (EasyList, EasyPrivacy, Fanboy Annoyance / Social Blocking)\n"
 # Download AdBlock Lists (EasyList, EasyPrivacy, Fanboy Annoyance / Social Blocking)
-curl -s -L https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt https://easylist.to/easylist/fanboy-annoyance.txt https://easylist.to/easylist/fanboy-social.txt https://easylist.to/easylistgermany/easylistgermany.txt https://easylist-downloads.adblockplus.org/liste_fr.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt https://easylist-downloads.adblockplus.org/easyprivacy.tpl https://fanboy.co.nz/r/fanboy-complete.txt https://fanboy.co.nz/fanboy-antifacebook.txt > adblock.unsorted
+curl -s -L https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt https://easylist.to/easylist/fanboy-annoyance.txt https://easylist.to/easylist/fanboy-social.txt https://easylist.to/easylistgermany/easylistgermany.txt https://easylist-downloads.adblockplus.org/easyprivacy.tpl https://fanboy.co.nz/r/fanboy-complete.txt https://fanboy.co.nz/fanboy-antifacebook.txt > adblock.unsorted
 
 
 printf "Look for: ||domain.tld^\n"
